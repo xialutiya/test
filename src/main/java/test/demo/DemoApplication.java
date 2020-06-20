59fbf24dbb24ca351fbcc3781a6596972613b50f
lds <>
2020-06-20 08:52:58 +0800

    测试

--- src/main/java/test/demo/DemoApplication.java
+++ src/main/java/test/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package test.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * ceshi.
+ */
 @SpringBootApplication
 public class DemoApplication {
 
